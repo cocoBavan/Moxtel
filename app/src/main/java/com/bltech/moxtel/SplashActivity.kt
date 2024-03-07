ae443972993a93b77faa30d936de510da81d7ce3
Bavan Palan <tuberslab@gmail.com>
2024-03-07 21:15:09 +1100

    MOX-001-Splash-Screen: Added AVD for Splashscreen

---
+++ app/src/main/java/com/bltech/moxtel/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.bltech.moxtel
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity: Activity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().also {
+            it.setKeepOnScreenCondition { true }
+        }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
