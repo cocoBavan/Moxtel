fb4b3021d97d66f979ade4a9e8cd7ae251a95533
Bavan Palan <tuberslab@gmail.com>
2024-03-08 13:19:54 +1100

    MOX-002 : Dependencies cleaned up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,36 +52,42 @@ android {
 }
 
 dependencies {
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.lifeCycleRuntime)
+    implementation(Dependencies.composeActivity)
+    implementation(platform(Dependencies.composeBom))
+    implementation(Dependencies.composeUI)
+    implementation(Dependencies.composeUIGraphics)
+    implementation(Dependencies.composeUIToolingPreview)
+    implementation(Dependencies.composeMaterial)
+    implementation(Dependencies.composeNavigation)
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.retrofitGson)
+    implementation(Dependencies.httpLoggingInterceptor)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation(Dependencies.coreSplash)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(Dependencies.hiltNavigation)
 
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+    implementation(Dependencies.coroutines)
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("org.mockito:mockito-core:2.19.0")
+    debugImplementation(Dependencies.composeUITooling)
+    debugImplementation(Dependencies.uiTestManifest)
 
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    testImplementation(Dependencies.unitTest)
+
+    androidTestImplementation(Dependencies.uiTest)
+    androidTestImplementation(Dependencies.uiTestExpresso)
+    androidTestImplementation(platform(Dependencies.composeBom))
+    androidTestImplementation(Dependencies.uiTestCompose)
+
+    implementation(Dependencies.hilt)
+    kapt(Dependencies.hiltCompiler)
+
+    testImplementation(Dependencies.unitTest)
+    testImplementation(Dependencies.unitTestMockk)
+    testImplementation(Dependencies.unitTestMockito)
 }
 
 kapt {
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,22 @@
+object Versions {
+    const val SPLASH = "1.0.1"
+    const val COROUTINES = "1.7.1"
+    const val RETROFIT = "2.9.0"
+    const val HTTP_LOGGING_INTERCEPTOR = "4.12.0"
+    const val COMPOSE_NAVIGATION = "2.7.7"
+    const val COMPOSE_BOM = "2024.02.02"
+    const val COMPOSE_ACTIVITY = "1.8.2"
+    const val HILT = "2.49"
+    const val HILT_NAVIGATION = "1.2.0"
+
+    const val JUNIT = "4.13.2"
+    const val MOCKK = "1.12.3"
+    const val MOCKITO = "2.19.0"
+
+    const val UITEST = "1.1.5"
+    const val UITEST_EXPRESSO = "3.5.1"
+
+    const val CORE_KTX = "1.12.0"
+
+    const val LIFE_CYCLE = "2.7.0"
+}
