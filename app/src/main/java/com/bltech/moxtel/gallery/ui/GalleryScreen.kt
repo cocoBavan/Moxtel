f8037c8bfa1fd6481fb5aa1b8d85b9895f670bf7
Bavan Palan <tuberslab@gmail.com>
2024-03-09 14:45:48 +1100

    BUX-002- Gallery Repository is based on an Interface to enable Preview and Testing.

--- app/src/main/java/com/bltech/moxtel/gallery/ui/GalleryScreen.kt
+++ app/src/main/java/com/bltech/moxtel/gallery/ui/GalleryScreen.kt
@@ -1,5 +1,6 @@
 package com.bltech.moxtel.gallery.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,7 +14,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,17 +31,23 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import com.bltech.moxtel.R
+import com.bltech.moxtel.gallery.data.IGalleryRepository
+import com.bltech.moxtel.gallery.data.model.GalleryResponse
+import com.bltech.moxtel.gallery.data.model.GitHubMovie
 import com.bltech.moxtel.gallery.ui.model.GalleryMovieModel
 import com.bltech.moxtel.gallery.ui.model.GalleryUIState
 import com.bltech.moxtel.global.navigation.MoxRoutes
+import com.bltech.moxtel.global.ui.theme.MoxtelTheme
 
 @Composable
 fun GalleryScreen(viewModel: GalleryViewModel = hiltViewModel(), navController: NavController) {
@@ -49,8 +59,8 @@ fun GalleryScreen(viewModel: GalleryViewModel = hiltViewModel(), navController: 
             Text(text = "Loading...")
         }
 
-        GalleryUIState.Error -> {
-            Text(text = "Error...")
+        is GalleryUIState.Error -> {
+            Text(text = "Error: ${uiState.detail}")
         }
 
         is GalleryUIState.Success -> {
@@ -76,69 +86,110 @@ fun GalleryView(movies: List<GalleryMovieModel>, navController: NavController) {
 
 @Composable
 fun GalleryItemView(movie: GalleryMovieModel, modifier: Modifier = Modifier) {
-    Box(
-        modifier
-            .aspectRatio(ratio = 1f)
+    Card(
+        modifier = modifier
             .padding(dimensionResource(id = R.dimen.galley_view_text_box_padding))
-            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.galley_view_cell_rounded_corner)))
-            .background(Color.Gray)
-            .fillMaxSize()
             .border(
                 width = dimensionResource(id = R.dimen.galley_view_cell_border_size),
                 color = Color.Black,
                 shape = RoundedCornerShape(dimensionResource(id = R.dimen.galley_view_cell_rounded_corner))
-            ),
-        contentAlignment = Alignment.BottomStart
-
+            )
     ) {
-        SubcomposeAsyncImage(
-            modifier = Modifier
-                .aspectRatio(ratio = 1f),
-            model = movie.posterUrl,
-            contentDescription = stringResource(
-                id = R.string.galley_view_item_content_description,
-                movie.title
-            ),
-            contentScale = ContentScale.FillWidth,
-            clipToBounds = true,
+        Box(
+            modifier
+                .aspectRatio(ratio = 1f)
+                .clip(RoundedCornerShape(dimensionResource(id = R.dimen.galley_view_cell_rounded_corner)))
+                .fillMaxSize(),
+            contentAlignment = Alignment.BottomStart
+
         ) {
-            val state = painter.state
-            when (state) {
-                is AsyncImagePainter.State.Loading -> {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(Modifier.size(dimensionResource(id = R.dimen.galley_view_cell_progress_view_size)))
+            SubcomposeAsyncImage(
+                modifier = Modifier
+                    .aspectRatio(ratio = 1f),
+                model = movie.posterUrl,
+                contentDescription = stringResource(
+                    id = R.string.galley_view_item_content_description,
+                    movie.title
+                ),
+                contentScale = ContentScale.FillWidth,
+                clipToBounds = true,
+            ) {
+                val state = painter.state
+                when (state) {
+                    is AsyncImagePainter.State.Loading -> {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(Modifier.size(dimensionResource(id = R.dimen.galley_view_cell_progress_view_size)))
+                        }
+                    }
+
+                    is AsyncImagePainter.State.Error -> {
+                        Image(
+                            painter = painterResource(id = R.drawable.movie_placeholder),
+                            contentDescription = stringResource(
+                                id = R.string.galley_view_item_content_description,
+                                movie.title
+                            )
+                        )
+                    }
+
+                    else -> {
+                        SubcomposeAsyncImageContent()
                     }
                 }
-
-                is AsyncImagePainter.State.Error -> {
-                    Image(
-                        painter = painterResource(id = R.drawable.movie_placeholder),
-                        contentDescription = stringResource(
-                            id = R.string.galley_view_item_content_description,
-                            movie.title
-                        )
-                    )
-                }
-
-                else -> {
-                    SubcomposeAsyncImageContent()
-                }
             }
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White.copy(alpha = 0.6f))
-                .padding(dimensionResource(id = R.dimen.galley_view_text_box_padding))
-        ) {
-            Text(
-                text = movie.title,
-                fontSize = dimensionResource(id = R.dimen.galley_view_title_size).value.sp,
-                fontWeight = FontWeight.Bold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White.copy(alpha = 0.6f))
+                    .padding(dimensionResource(id = R.dimen.galley_view_text_box_padding))
+            ) {
+                Text(
+                    text = movie.title,
+                    fontSize = dimensionResource(id = R.dimen.galley_view_title_size).value.sp,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
 
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Composable
+fun GreetingPreview() {
+    MoxtelTheme {
+        val navController = rememberNavController()
+        GalleryScreen(
+            viewModel = GalleryViewModel(repository = object : IGalleryRepository {
+                override suspend fun getMovies() =
+                    GalleryResponse(
+                        movies = (0..100).map {
+                            GitHubMovie(
+                                id = it,
+                                posterUrl = "https://picsum.photos/200/300",
+                                title = "Age Of Empires $it"
+                            )
+                        }
+                    )
+
+                override suspend fun getMovie(id: Int): GitHubMovie? = null
+            }),
+            navController = navController
+        )
     }
 }
