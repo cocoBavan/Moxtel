a5673f55a817989a487de0a61a129aeed0881ccf
Bavan Palan <tuberslab@gmail.com>
2024-03-09 14:45:48 +1100

    BUX-002- Gallery Repository is based on an Interface to enable Preview and Testing.

--- app/src/main/java/com/bltech/moxtel/gallery/ui/GalleryViewModel.kt
+++ app/src/main/java/com/bltech/moxtel/gallery/ui/GalleryViewModel.kt
@@ -2,7 +2,7 @@ package com.bltech.moxtel.gallery.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bltech.moxtel.gallery.data.GalleryRepository
+import com.bltech.moxtel.gallery.data.IGalleryRepository
 import com.bltech.moxtel.gallery.data.model.GitHubMovie
 import com.bltech.moxtel.gallery.ui.model.GalleryMovieModel
 import com.bltech.moxtel.gallery.ui.model.GalleryUIState
@@ -18,13 +18,13 @@ import javax.inject.Inject
 //@Stable
 @HiltViewModel
 class GalleryViewModel(
-    private val repository: GalleryRepository,
+    private val repository: IGalleryRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) :
     ViewModel() {
 
     @Inject
-    constructor(repository: GalleryRepository) : this(repository, Dispatchers.IO)
+    constructor(repository: IGalleryRepository) : this(repository, Dispatchers.IO)
 
     private var _moviesFlow = MutableStateFlow<GalleryUIState>(GalleryUIState.Loading)
     val movieFlow = _moviesFlow.asStateFlow()
@@ -36,17 +36,17 @@ class GalleryViewModel(
                 if (!movies.isNullOrEmpty()) {
                     _moviesFlow.value = GalleryUIState.Success(movies)
                 } else {
-                    _moviesFlow.value = GalleryUIState.Error
+                    _moviesFlow.value = GalleryUIState.Error("Movie Response is null or Empty")
                 }
             } catch (e: Exception) {
                 print(e.stackTrace)
-                _moviesFlow.value = GalleryUIState.Error
+                _moviesFlow.value = GalleryUIState.Error(e.localizedMessage ?: " Unknown Exception")
             }
         }
     }
-    
+
     private val mapperDataToUI: (GitHubMovie) -> GalleryMovieModel? = { movie ->
-        if (movie.id != null && movie.title != null && movie.posterUrl != null && movie.year != null) {
+        if (movie.id != null && movie.title != null && movie.posterUrl != null) {
             GalleryMovieModel(
                 id = movie.id,
                 title = movie.title,
--- app/src/main/java/com/bltech/moxtel/gallery/ui/model/GalleryUIState.kt
+++ app/src/main/java/com/bltech/moxtel/gallery/ui/model/GalleryUIState.kt
@@ -2,6 +2,6 @@ package com.bltech.moxtel.gallery.ui.model
 
 sealed class GalleryUIState {
     data class Success(var movies: List<GalleryMovieModel>) : GalleryUIState()
-    data object Error : GalleryUIState()
+    data class Error(var detail: String) : GalleryUIState()
     data object Loading : GalleryUIState()
 }
