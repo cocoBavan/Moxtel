3ccecbcde2dfa32a3380f27f52eb1f4f75653081
Bavan Palan <tuberslab@gmail.com>
2024-03-09 22:27:19 +1100

    BUX-006: Clean up Details Screen

--- app/src/main/java/com/bltech/moxtel/features/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/bltech/moxtel/features/ui/details/DetailsScreen.kt
@@ -3,12 +3,9 @@ package com.bltech.moxtel.features.ui.details
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,27 +16,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImagePainter
-import coil.compose.SubcomposeAsyncImage
-import coil.compose.SubcomposeAsyncImageContent
 import com.bltech.moxtel.R
 import com.bltech.moxtel.features.data.model.GitHubMovie
 import com.bltech.moxtel.features.ui.MovieCellView
@@ -47,6 +36,7 @@ import com.bltech.moxtel.features.ui.details.model.DetailsUIState
 import com.bltech.moxtel.features.ui.home.model.MovieCellModel
 import com.bltech.moxtel.global.navigation.MoxRoutes
 import com.bltech.moxtel.global.theme.MoxtelTheme
+import com.bltech.moxtel.global.util.unwrapped
 
 
 @Composable
@@ -84,96 +74,80 @@ fun DetailsView(
         verticalArrangement = Arrangement.spacedBy(10.dp),
         modifier = Modifier.verticalScroll(rememberScrollState())
     ) {
-        SubcomposeAsyncImage(
-            modifier = Modifier
-                .aspectRatio(ratio = 16 / 9.0f)
-                .fillMaxWidth(),
-            model = movie.posterUrl,
-            contentDescription = stringResource(
-                id = R.string.galley_view_item_content_description,
-                movieTitle
-            ),
-            contentScale = ContentScale.FillWidth,
-            clipToBounds = true,
-        ) {
-            val state = painter.state
-            when (state) {
-                is AsyncImagePainter.State.Loading -> {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(Modifier.size(dimensionResource(id = R.dimen.galley_view_cell_progress_view_size)))
-                    }
-                }
-
-                is AsyncImagePainter.State.Error -> {
-                    Image(
-                        painter = painterResource(id = R.drawable.movie_placeholder),
-                        contentDescription = stringResource(
-                            id = R.string.galley_view_item_content_description,
-                            movieTitle
-                        )
-                    )
-                }
-
-                else -> {
-                    SubcomposeAsyncImageContent()
-                }
-            }
-        }
+        HeroImage(movie = movie)
         Text(text = movieTitle)
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            Image(
-                modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.hd_icon),
-                contentDescription = ""
-            )
-            Image(
-                modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.cc_icon),
-                contentDescription = ""
-            )
-        }
-        Button(
-            modifier = Modifier
-                .fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-            shape = RoundedCornerShape(0.dp),
-            onClick = { }) {
-            Text("Watch Now")
-        }
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-            shape = RoundedCornerShape(0.dp),
-            onClick = {}) {
-            Image(
-                modifier = Modifier.size(12.dp),
-                painter = painterResource(id = R.drawable.plus_icon),
-                contentDescription = "",
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer)
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Text("Watchlist")
-        }
+        AvailableFeatures()
+        WatchNowButton()
+        AddToWatchListButton()
+        Text(text = movie.plot.unwrapped)
+        SimilarMovies(similarMovies, navController)
+    }
+}
 
-        Text(text = movie.plot ?: "")
+@Composable
+fun AvailableFeatures() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Image(
+            modifier = Modifier.size(30.dp),
+            painter = painterResource(id = R.drawable.hd_icon),
+            contentDescription = ""
+        )
+        Image(
+            modifier = Modifier.size(30.dp),
+            painter = painterResource(id = R.drawable.cc_icon),
+            contentDescription = ""
+        )
+    }
+}
 
-        if (similarMovies.isNotEmpty()) {
-            Text(
-                text = "Similar Movies",
-                style = MaterialTheme.typography.titleMedium
-            )
-            LazyRow {
-                items(similarMovies) {
-                    MovieCellView(movie = it, modifier = Modifier
-                        .width(200.dp)
-                        .clickable {
-                            navController.navigate("${MoxRoutes.DETAILS}/${it.id}")
-                        })
-                }
+@Composable
+fun SimilarMovies(similarMovies: List<MovieCellModel>, navController: NavHostController) {
+    if (similarMovies.isNotEmpty()) {
+        Text(
+            text = "Similar Movies",
+            style = MaterialTheme.typography.titleMedium
+        )
+        LazyRow {
+            items(similarMovies) {
+                MovieCellView(movie = it, modifier = Modifier
+                    .width(200.dp)
+                    .clickable {
+                        navController.navigate("${MoxRoutes.DETAILS}/${it.id}")
+                    })
             }
         }
+    }
+}
+
+@Composable
+fun WatchNowButton() {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+        shape = RoundedCornerShape(0.dp),
+        onClick = { }) {
+        Text("Watch Now")
+    }
+}
+
+@Composable
+fun AddToWatchListButton() {
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+        shape = RoundedCornerShape(0.dp),
+        onClick = {}) {
+        Image(
+            modifier = Modifier.size(12.dp),
+            painter = painterResource(id = R.drawable.plus_icon),
+            contentDescription = "",
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text("Watchlist")
     }
 }
 
@@ -191,3 +165,5 @@ fun DetailsViewPreview() {
         )
     }
 }
+
+
