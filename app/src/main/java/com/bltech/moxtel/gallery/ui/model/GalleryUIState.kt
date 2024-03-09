3d45a1f843330810cb0736d6e8a15546c6733108
Bavan Palan <tuberslab@gmail.com>
2024-03-09 18:51:05 +1100

    MOX-005: Details Screen UI  layout has been completed

--- app/src/main/java/com/bltech/moxtel/gallery/ui/model/GalleryUIState.kt
+++ app/src/main/java/com/bltech/moxtel/gallery/ui/model/GalleryUIState.kt
@@ -1,7 +1,7 @@
 package com.bltech.moxtel.gallery.ui.model
 
 sealed class GalleryUIState {
-    data class Success(var movies: List<GalleryMovieModel>) : GalleryUIState()
+    data class Success(var movies: List<MovieCellDataModel>) : GalleryUIState()
     data class Error(var detail: String) : GalleryUIState()
     data object Loading : GalleryUIState()
 }
