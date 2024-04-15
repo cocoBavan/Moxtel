e0170f09d5d716039a82e63790a8edbc5773ba29
Bavan Palan <tuberslab@gmail.com>
2024-04-15 12:55:12 +1000

    MOX-010: Added use case to explain clean architecure

--- app/src/main/java/com/bltech/moxtel/global/di/MoxtelModule.kt
+++ app/src/main/java/com/bltech/moxtel/global/di/MoxtelModule.kt
@@ -8,6 +8,8 @@ import com.bltech.moxtel.features.data.datasource.local.MovieDao
 import com.bltech.moxtel.features.data.datasource.remote.MoxtelGitHubService
 import com.bltech.moxtel.features.data.repository.MoviesRepository
 import com.bltech.moxtel.features.domain.contract.IMovieRepository
+import com.bltech.moxtel.features.domain.usecase.FetchMoviesUseCase
+import com.bltech.moxtel.features.domain.usecase.IFetchMoviesUseCase
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
@@ -25,6 +27,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class MoxtelModule {
     @Provides
+    @Singleton
     fun providesGson(): Gson {
         return GsonBuilder()
             .create()
@@ -33,6 +36,7 @@ class MoxtelModule {
     @Provides
     fun providesHTTPClient(): OkHttpClient {
         return OkHttpClient.Builder()
+            //Add cache?
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BASIC
             })
@@ -48,6 +52,7 @@ class MoxtelModule {
             .build()
     }
 
+    @Singleton
     @Provides
     fun providesGitHubService(client: OkHttpClient, gson: Gson): MoxtelGitHubService {
         return getGitHubRetrofit(BuildConfig.BASE_URL, client, gson)
@@ -70,8 +75,14 @@ class MoxtelModule {
     }
 
     @Provides
+    @Singleton
     fun providesMovieDao(appDatabase: MovieDB): MovieDao {
         return appDatabase.movieDao()
+    }
+
+    @Provides
+    fun providesFetchMoviesUseCase(useCase: FetchMoviesUseCase): IFetchMoviesUseCase {
+        return useCase
     }
 
 }
